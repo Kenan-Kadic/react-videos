0dc824fbdefdf106b9293a6c37c07e81f50c767e
Kenan Kadic <ken.kadic@outlook.com>
2021-12-22 12:38:23 -0600

    VideoList added

--- src/apis/YouTube.js
+++ src/apis/YouTube.js
@@ -1,1 +1,14 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyBFI_brtAnsg1U8MEJe9EEpHv8xqwlrtjk'
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY,
+
+    }
+});
---
+++ src/components/VideoList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const VideoList = () => {
+    
+    return (
+        <div>
+            Videolist        
+        </div>
+    )
+}
+
+export default VideoList
